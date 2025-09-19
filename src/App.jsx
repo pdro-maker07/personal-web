f5be173f6e7c949f78a99e3273df90b863a97a09
pdro-maker07 <putra.timur804@gmail.com>
2025-09-20 02:43:15 +0800

    Deskripsi singkat tentang perubahan yang Anda buat

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,34 @@ function App() {
         </div>
         <img src={DataImage.HeroImage} alt="Hero Image" className="w-[500px] md:ml-auto" />
       </div>
+
+      {/*tentang */}
+      <div className="tentang mt-32 py-10">
+        <div className="xl:w-2/3 lg:w-3/4 w-full mx-auto p-7 bg-zinc-800 rounded-lg">
+        <img src={DataImage.HeroImage} alt="Image" className="w-12 rounded-md mb-10 sm:hidden" />
+          <p className="text-base/loose mb-10">
+          Hi, perkenalkan saya Pedrof Da Kristof, seorang full Stack Web Developer dan Desaigner untuk UI/UX Design maupun Product Digital, sehingga setiap proyek yang saya kembangkan tidak hanya terlihat menarik tetapi memberikan pengalaman pengguna yang optimal.
+          </p>
+          <div className=" flex items-center justify-between">
+            <img src={DataImage.HeroImage} alt="Image" className="w-12 rounded-md sm:block hidden" />
+            <div className="flex items-center gap-6">
+              <div>
+                <h1 className="text-4xl mb-1">
+                  45<span className="text-violet-500">+</span>
+                </h1>
+                <p>Proyek Selesai</p>
+              </div>
+              <div>
+                <h1 className="text-4xl mb-1">
+                  4<span className="text-violet-500">+</span>
+                </h1>
+                <p>Tahun Pengalaman</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/*tentang */}
     </>
   )
 }
