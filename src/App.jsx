cc5cc05ed1782e18a97a44e1e39574890e73edf3
pdro-maker07 <putra.timur804@gmail.com>
2025-09-20 15:11:35 +0800

    ditambahkan

--- src/App.jsx
+++ src/App.jsx
@@ -21,9 +21,9 @@ function App() {
       {/*tentang */}
       <div className="tentang mt-32 py-10">
         <div className="xl:w-2/3 lg:w-3/4 w-full mx-auto p-7 bg-zinc-800 rounded-lg">
-        <img src={DataImage.HeroImage} alt="Image" className="w-12 rounded-md mb-10 sm:hidden" />
+          <img src={DataImage.HeroImage} alt="Image" className="w-12 rounded-md mb-10 sm:hidden" />
           <p className="text-base/loose mb-10">
-          Hi, perkenalkan saya Pedrof Da Kristof, seorang full Stack Web Developer dan Desaigner untuk UI/UX Design maupun Product Digital, sehingga setiap proyek yang saya kembangkan tidak hanya terlihat menarik tetapi memberikan pengalaman pengguna yang optimal.
+            Hi, perkenalkan saya Pedrof Da Kristof, seorang full Stack Web Developer dan Desaigner untuk UI/UX Design maupun Product Digital, sehingga setiap proyek yang saya kembangkan tidak hanya terlihat menarik tetapi memberikan pengalaman pengguna yang optimal.
           </p>
           <div className=" flex items-center justify-between">
             <img src={DataImage.HeroImage} alt="Image" className="w-12 rounded-md sm:block hidden" />
@@ -39,6 +39,20 @@ function App() {
                   4<span className="text-violet-500">+</span>
                 </h1>
                 <p>Tahun Pengalaman</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="tools mt-32">
+          <h1 className="text-4xl/snug font-bold mb-4">Tools yang dipakai</h1>
+          <p className="w-2/5 text-base/loose opacity-50">Berikut ini beberapa tools yang biasa saya pakai untuk pembuatan Wbsite ataupun Design</p>
+          <div className="tools-box">
+            <div className="flex items-center gap-2 p-3 border border-x-zinc-600 rounded-md hover:bg-zinc-800 group">
+              <img src="" alt="Tools Image" className="w-14 bg-zinc-800 p-1 group-hover:bg-zinc-900" />
+              <div>
+                <h4>Nama Tools</h4>
+                <p>Design App</p>
               </div>
             </div>
           </div>
